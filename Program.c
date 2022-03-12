7ab833928b1c00aaf10e77545bfed19caecefae5
ignurof <ignurof@outlook.com>
2022-03-12 16:33:11 +0100

    init

---
+++ Program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int num;
+	
+	printf("Find out if the input number is even or odd!\n\n");
+	printf("Enter number: ");
+	// Receive from input stream, specifies int format, using & to specify the adress to the variable
+	scanf_s("%d", &num);
+
+	// % modulo division meaning what is remaining after division of num / 2
+	if (num % 2 == 0)
+		printf("%d is even\n", num);
+	else
+		printf("%d is odd\n", num);
+
+	return 0;
+}
